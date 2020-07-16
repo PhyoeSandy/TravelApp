84d07815267dc2c9a9b4eff8b54864e19faaea01
PhyoeSandy <phyoesandypink123@gmail.com>
2020-07-16 17:01:58 +0630

    feat: viewHolder added

---
+++ app/src/main/java/com/padcmyanmar/padcx/androidarchitecturepatterns/views/viewholder/BaseTravelViewHolder.kt
@@ -0,0 +1,11 @@
+package com.padcmyanmar.padcx.androidarchitecturepatterns.views.viewholder
+
+import android.view.View
+import com.padcmyanmar.padcx.androidarchitecturepatterns.data.vos.ToursVO
+
+/**
+ * Created by Phyoe Sandy Soe Tun
+ * on 2/20/2020.
+ */
+abstract class BaseTravelViewHolder(itemView: View) : BaseViewHolder<ToursVO>(itemView) {
+}
---
+++ app/src/main/java/com/padcmyanmar/padcx/androidarchitecturepatterns/views/viewholder/CountryViewHolder.kt
@@ -0,0 +1,46 @@
+package com.padcmyanmar.padcx.androidarchitecturepatterns.views.viewholder
+
+import android.util.Log
+import android.view.View
+import com.bumptech.glide.Glide
+import com.padcmyanmar.padcx.androidarchitecturepatterns.data.vos.ToursVO
+import com.padcmyanmar.padcx.travelapp.delegates.ToursItemDelegate
+import kotlinx.android.synthetic.main.item_country.view.*
+
+/**
+ * Created by Phyoe Sandy Soe Tun
+ * on 2/20/2020.
+ */
+
+class CountryViewHolder(itemView: View,private val delegate: ToursItemDelegate) : BaseTravelViewHolder(itemView) {
+
+    init{
+        itemView.setOnClickListener {
+            Log.d("M data is ",mData.toString())
+            mData?.let {
+                delegate.onTapToursItem(it.name)
+            }
+        }
+
+        itemView.ivCountry.setOnClickListener {
+            mData?.let {
+                delegate.onTapToursItem(it.name)
+            }
+        }
+    }
+
+    override fun bindData(data: ToursVO) {
+        mData = data
+        Glide.with(itemView.context)
+            .load(data.photos[0])
+            .into(itemView.ivCountry)
+        itemView.tvLocation.text = data.location
+        itemView.tvMaxTour.text = data.averageRating.toString()
+
+        itemView.ivCountry.setOnClickListener {
+            mData?.let {
+                delegate.onTapToursItem(it.name)
+            }
+        }
+    }
+}
